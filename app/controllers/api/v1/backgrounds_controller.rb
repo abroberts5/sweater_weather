4613152dd42a58839404b33d14d6c1c682482503
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2019-02-27 11:28:14 -0700

    adds additional functionality for default image on background

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,6 +2,11 @@ class Api::V1::BackgroundsController < ApplicationController
 
   def index
     image = Photo.new(params[:location]).grab_photo
-    render json: { photo: image }, status: 201
+    if image!= nil
+      render json: { photo: image }, status: 201
+    else
+      default = "https://www.rd.com/wp-content/uploads/2018/02/00_Can-You-Guess-the-Famous-City-Based-on-Their-Skylines_441608320_Funny-Solution-Studio_FT.jpg"
+      render json: { default_image: default }, status: 202
+    end
   end
 end

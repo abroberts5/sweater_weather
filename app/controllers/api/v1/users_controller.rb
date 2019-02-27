ac31010ecee5c80c6be247cffc9046c543ca42ad
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2019-02-27 14:30:12 -0700

    updated key generation method on user controller. was generating new key each time.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,20 @@
 class  Api::V1::UsersController < ApplicationController
 
   def create
+    key = key_generation
     if params[:password] == params[:password_confirmation]
       user = User.create({email: params["email"], password: params[:password],
       password_confirmation: params[:password_confirmation],
-      api_key: SecureRandom.urlsafe_base64})
+      api_key: key})
       render json: { api_key: user.api_key }, status: 201
     else
       render json: {error: "Password and Confirmation not the same"}, status: 401
     end
   end
-
 end
+
+  private
+
+  def key_generation
+    SecureRandom.urlsafe_base64
+  end
